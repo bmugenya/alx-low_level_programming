cc772b63c269130c21fcd87d8b9428ee5c9dd8ec
bmugenya <bmugenya26@gmail.com>
2023-02-17 11:29:17 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+  int x;
+  for (x ='a'; x <= 'z'; x++)
+    putchar(x);
+  for (x = 'A'; x <= 'Z'; x++)
+    putchar(x);
+  putchar('\n');
+  return (0);
+
+}
