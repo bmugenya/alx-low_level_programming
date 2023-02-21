d729956b3530b08b69d0f71e1b44426d96253539
bmugenya <bmugenya26@gmail.com>
2023-02-21 08:45:36 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,12 +9,18 @@
 int _print_sign(int x)
 {
   if (x == 0)
+    {
     _putchar('0');
     return (0);
+    }
   else if(x < 0)
+    {
     _putchar('-');
     return (-1);
+    }
   else
-    _putchar('+')
-    return (1);
+    {
+     _putchar('+');
+     return (1);
+    }
 }
