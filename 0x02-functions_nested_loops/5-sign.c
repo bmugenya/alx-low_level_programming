3c55d502c29ddfabb7b99e1f0249d07948d59b9f
bmugenya <bmugenya26@gmail.com>
2023-02-21 08:49:07 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _print_sign - prints the sign of a number
+ * print_sign - prints the sign of a number
  * @x : number to be assigned
  * 
  * Return: 1 if number is positive, 0if number is 0, and -1 is negative */
 
-int _print_sign(int x)
+int print_sign(int x)
 {
   if (x == 0)
     {
