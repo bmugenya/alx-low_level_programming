2f94ada68608a06783ca07a2122c5ed1f7835209
bmugenya <bmugenya26@gmail.com>
2023-02-22 12:41:56 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n 70 98
+ * @n: natural number 
+ */
+
+void print_to_98(int x)
+
+  if (x >= 0 && x <= 98)
+    {
+      _putchar(x);
+      _putchar(',');
+      _putchar(' ');
+    }
+}
