fc0ee26e946764466c1ce7bdfee761ea65466fa8
bmugenya <bmugenya26@gmail.com>
2023-02-24 05:41:54 -0800

    update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,16 @@ int main(void)
       int x;
 
     for (x = 1; x <= 100; x++)
+      {
 	if (x % 3 == 0)
-	    printf("FIZZ ");
+	    printf("FIZZ");
 	else if(x % 5 == 0)
-	    printf("BUZZ ");
+	    printf("BUZZ");
 	else if(x % 3 == 0 && x % 5 == 0)
-	    printf("FIZZBUZZ ");
+	    printf("FIZZBUZZ");
 	else
-	    printf("%d ", x);
+	    printf("%d", x);
+	printf(" ");
+      }
     printf("\n");
 }
