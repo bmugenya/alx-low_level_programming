d89589583ed090277dcec6dd4373dd356066f79f
bmugenya <bmugenya26@gmail.com>
2023-02-27 11:19:58 -0800

    This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,7 +9,7 @@ void _strlen(char *s)
 {
   int str_length;
 
-  for(str_lenth = 0; s[str_length] != '\0'; ++str_length);
+  for(str_length = 0; s[str_length] != '\0'; ++str_length);
 
   return (str_length);
 }
