5db4f49797eb36b0b364dc7b29a72a8162cb934b
bmugenya <bmugenya26@gmail.com>
2023-03-06 16:49:10 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,9 +12,8 @@ char *_memset(char *s, char b, unsigned int n)
 {
   unsigned int index;
 
-  for (index = 0; index < n; ++index)
-    {
-      *(s + index) = b;
-    }
+  for (index = 0; index < n; index++)
+    s[index] = b;
+    
   return (s);
 }
