de847f2eff1606a0bc1d2ac1819063edb57e80c0
bmugenya <bmugenya26@gmail.com>
2023-03-06 17:19:44 -0800

    strbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence in the string s
+ * @s: a string 
+ * @accept: accepted bytes in the string
+ * Return: pointer to the byte in a string
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+  int index, x;
+
+  for (index = 0; s[index] != '\0'; index++)
+    {
+      for (x = 0; accept[x] != '\0'; x++)
+	{
+	  if (s[index] == x[accept])
+	    return (s[index]);
+	}
+    }
+  return ('\0');
+}
