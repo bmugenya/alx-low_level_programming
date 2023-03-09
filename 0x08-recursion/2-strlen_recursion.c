82c06aefe4b5228808b8044c84e9301441fc8a9a
bmugenya <bmugenya26@gmail.com>
2023-03-09 11:06:29 -0800

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - output length of characters and strings 
+ * @s: character to be outpute in the stdout
+ */
+
+void _strlen_recursion(char *s)
+{
+
+  if (*s == '\0')
+    return (0);
+
+  s++;
+  return (_strlen_recursion(++s));
+ 
+
+}
